785c8ed824554d59ce1e991f040bf8a103bb78e0
huiboxes <huiboxes@gmail.com>
2021-05-14 11:55:14 +0800

    完成页脚组件

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    navfooter
+  <div class="footer">
+    <div class="footer-logo">
+      <img src="/imgs/logo-footer.png" alt="" />
+      <p>嗨商城</p>
+    </div>
+    <div class="footer-link">
+      <a href="https://juejin.cn/user/2858385965326968"
+        >掘金账号<span>|</span></a
+      >
+      <a href="">high服务（BaaS）<span>|</span></a>
+      <a href="">high运维<span>|</span></a>
+      <a href="">high课堂</a>
+    </div>
+    <div class="copyright">
+      Copyright ©2020 jutalent.com All Rights Reserved.
+    </div>
   </div>
 </template>
 
@@ -10,4 +24,30 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import './../assets/scss/config.scss';
+.footer {
+  height: 234px;
+  border-top: 4px solid $colorA;
+  background-color: $colorB;
+  color: $colorD;
+  text-align: center;
+  .footer-logo{
+    margin-top: 46px;
+    margin-bottom: 31px;
+    img{
+      margin-bottom: 13px;
+    }
+  }
+  .footer-link{
+    a{
+      color: $colorD;
+      display: inline-block;
+    }
+    span{
+      margin: 0 10px;
+    }
+    margin-bottom: 13px;
+  }
+}
+</style>
